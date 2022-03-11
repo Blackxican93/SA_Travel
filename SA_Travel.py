eb865f138d8ada49e46eb61fb3805689f987fc15
Vernon Stephens <Blackxican93@users.noreply.github.com>
2022-03-11 02:51:49 +0000

    Just need to add in some functions

--- SA_Travel.py
+++ SA_Travel.py
@@ -3,20 +3,11 @@
 import pandas as pd
 
 def main():
-    df = pd.read_excel('SA_Travel.xlsx', sheet_name = [0,1,2,3,4,5])
-    #pd.set_option("display.max_columns", 10, "display.max_rows", None)
-
-    # Define xls file
+    # Define xlsx file
     excel_file = 'SA_Travel.xlsx'
 
-    # create a DataFrame (DF) object
-    #travel = pd.read_excel(excel_file)
-
-    # show the first five rows of our DF
-    #print(travel.head())
-
-    # Choose the first column "Title" as
-    # index (index=0)
+    # Define sheets and ensure they are indexed
+    # df = pd.read_excel('SA_Travel.xlsx', sheet_name = [0,1,2,3,4,5])
     travel_sheet0 = pd.read_excel(excel_file, sheet_name=0, index_col=0)
     travel_sheet1 = pd.read_excel(excel_file, sheet_name=1, index_col=0)
     travel_sheet2 = pd.read_excel(excel_file, sheet_name=2, index_col=0)
@@ -25,13 +16,13 @@ def main():
     travel_sheet5 = pd.read_excel(excel_file, sheet_name=5, index_col=0)
 
     another_topic = "Y"
-
+    # While loop required to ask user if they want to explore more
     while another_topic == "Y":
-
+        # Promt user to select a topic they would like to explore
         print("So I hear you are visiting San Antonio, TX? Awesome!")
         print("Based on some research from TripAdvisor, I'll give you some top destinations based on topics you may want to explore.")
 
-        topics = ["0-Tours & Sightseeing", "1-Outdoor", "2-Shopping", "3-Historic", "4-Amusement & Museums", "5-Great Views"]
+        topics = ["0-Tours & Sightseeing", "1-Outdoors", "2-Shopping", "3-Historic", "4-Amusement & Museums", "5-Great Views"]
         print(topics)
 
         user_selection = int(input("Which topic would you like to explore? Pick a number.  "))
@@ -52,9 +43,9 @@ def main():
         else:
             print("Invalid response")
 
+        # Prompt the user if they want to choose another topic
         another_topic = (input("Would you like to explore another topic (Y/N)? "))    
 
-        #Prompt the user if they want to choose another topic
         if another_topic == "Y":
             print("Sounds good")
         elif another_topic == "N":
@@ -62,10 +53,7 @@ def main():
         else:
             print("Quitting SA_Travel")
 
-        # DF has 5 rows and 24 columns (indexed by title)
-        # print the top 10 movies in the dataframe
-        # print(travel_sheet1.head()) 
-
+        # DF has 5 rows and 24 columns (indexed by number
         # Export topics from the top dataframe to excel
         travel_sheet0.head().to_excel("Tours_and_Sightseeing.xlsx")
         travel_sheet1.head().to_excel("Outdoors.xlsx")
@@ -74,7 +62,7 @@ def main():
         travel_sheet4.head().to_excel("Amusement_and_Museums.xlsx")
         travel_sheet5.head().to_excel("Great_Views.xlsx")
 
-        # Export 5 movies from the top of the dataframe to json
+        # Export topics from the top of the dataframe to json
         travel_sheet0.head().to_json("Tours_and_Sightseeing.json")
         travel_sheet1.head().to_json("Outdoors.json")
         travel_sheet2.head().to_json("Shopping.json")
@@ -82,11 +70,11 @@ def main():
         travel_sheet4.head().to_json("Amusement_and_Museums.json")
         travel_sheet5.head().to_json("Great_Views.json")
 
-        # export 5 movies from the top of the dataframe to csv
+        # Export topics from the top of the dataframe to csv
         travel_sheet0.head().to_csv("Tours_and_Sightseeing.csv")
-        travel_sheet1.head().to_csv("Outdoors")
-        travel_sheet2.head().to_csv("Shopping")
-        travel_sheet3.head().to_csv("Historic")
+        travel_sheet1.head().to_csv("Outdoors.csv")
+        travel_sheet2.head().to_csv("Shopping.csv")
+        travel_sheet3.head().to_csv("Historic.csv")
         travel_sheet4.head().to_csv("Amusement_and_Museums.csv")
         travel_sheet5.head().to_csv("Great_Views.csv")
 
